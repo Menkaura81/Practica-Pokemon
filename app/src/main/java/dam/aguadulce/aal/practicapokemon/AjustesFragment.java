bc4426cf41f1c8492ade7c304b07367c64c8325d
Menkaura81 <bertoalarcon@gmail.com>
2025-01-15 19:01:45 +0100

    implementado cerrar sesion

--- app/src/main/java/dam/aguadulce/aal/practicapokemon/AjustesFragment.java
+++ app/src/main/java/dam/aguadulce/aal/practicapokemon/AjustesFragment.java
@@ -1,14 +1,20 @@
 package dam.aguadulce.aal.practicapokemon;
 
+import android.app.AlertDialog;
+import android.content.Intent;
 import android.os.Bundle;
-
 import androidx.fragment.app.Fragment;
+
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 import dam.aguadulce.aal.practicapokemon.databinding.FragmentAjustesBinding;
-import dam.aguadulce.aal.practicapokemon.databinding.FragmentMisPokemonsBinding;
 
 
 public class AjustesFragment extends Fragment {
@@ -25,14 +31,33 @@ public class AjustesFragment extends Fragment {
                              Bundle savedInstanceState) {
         binding = FragmentAjustesBinding.inflate(inflater, container, false);
 
-        binding.about.setOnClickListener(this::onAjustesSelected);
-
+        binding.about.setOnClickListener(v -> onAboutSelected());
+        binding.closeSession.setOnClickListener(v -> onCloseSessionSelected());
 
         return binding.getRoot();
     }
 
+    private void onCloseSessionSelected() {
+        // Se cierra sesión y se notifica al usuario
+        FirebaseAuth mAuth = FirebaseAuth.getInstance();
+        mAuth.signOut();
+        Toast.makeText(requireContext(), "Sesión cerrada", Toast.LENGTH_SHORT).show();
 
-    private boolean onAjustesSelected(){
-        return true;
+        // Volvemos a la pantalla de login
+        Intent i = new Intent(getContext(), LoginActivity.class);
+        startActivity(i);
+    }
+
+
+    private void onAboutSelected(){
+        TextView messageTextView = new TextView(requireContext());
+        messageTextView.setText(R.string.about_msg);
+        messageTextView.setGravity(Gravity.CENTER); // Center the text
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(requireContext());
+        builder.setTitle(R.string.about)
+                .setView(messageTextView)
+                .setPositiveButton(R.string.close_dialog, (dialog, which) -> dialog.dismiss())
+                .show();
     }
 }
